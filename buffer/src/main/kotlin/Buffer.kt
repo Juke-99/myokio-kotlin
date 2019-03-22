0b502fe89927be28b6b0a0fffe1474ad06c9e10d
Juke-99 <yanpahika@gmail.com>
2019-03-23 04:05:49 +0900

    add buffer

--- buffer/src/main/kotlin/Buffer.kt
+++ buffer/src/main/kotlin/Buffer.kt
@@ -1,5 +1,8 @@
 class Buffer {
-    public writeUtf8(strings: String, beginIndex: Int, endIndex: Int): Buffer {
-        
+    open fun writeUtf8(string: String, beginIndex: Int, endIndex: Int): Buffer {
+        if (beginIndex < 0) throw IllegalArgumentException("beginIndex < 0: " + beginIndex)
+        if (endIndex < beginIndex) throw IllegalArgumentException("endIndex < beginIndex: " + endIndex + " < " + beginIndex)
+        if (endIndex > string.length) throw IllegalArgumentException("endIndex > string.length: " + endIndex + " > " + string.length)
+        return Buffer()
     }
 }

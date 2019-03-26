f94ad0f665464656d3aba75499154e1e212b7abe
Juke-99 <yanpahika@gmail.com>
2019-03-27 02:26:32 +0900

    add SegmentPool class and take method

--- buffer/src/main/kotlin/Buffer.kt
+++ buffer/src/main/kotlin/Buffer.kt
@@ -1,4 +1,8 @@
 class Buffer {
+    var head: Segment? = null
+
+    init {}
+
     open fun writeUtf8(string: String, beginIndex: Int, endIndex: Int): Buffer {
         if (beginIndex < 0) throw IllegalArgumentException("beginIndex < 0: " + beginIndex)
         if (endIndex < beginIndex) throw IllegalArgumentException("endIndex < beginIndex: " + endIndex + " < " + beginIndex)
